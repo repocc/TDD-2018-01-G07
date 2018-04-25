d4cc2665fc10942faff273dc64d502efa6cac304
EstebanRodriguezUBA <rodriguezsigloxxi@gmail.com>
2018-04-24 22:01:13 -0300

    Se suben archivos con funcion -main y test para el mismo.  Realiza en esta funcionalidad primitiva un proceso batch.  Se puede a partir de ahora generar el jar como explico en README y testearlo (explicado tambien)

--- project.clj
+++ project.clj
@@ -1,6 +1,9 @@
-(defproject tp1-rule-validator "0.1.0-SNAPSHOT"
-  :description "Template for functional rule validation engine"
+(defproject motorClojure "1.0.0"
+  :description "Trabajo Practico Monitorizar eventos. Grupo 07"
   :url "http://materias.fi.uba.ar/7510/"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]])
+  :dependencies [[org.clojure/clojure "1.8.0"]]
+  :main ^skip-aot motor
+  :profiles {:uberjar {:aot :all}}
+  )
