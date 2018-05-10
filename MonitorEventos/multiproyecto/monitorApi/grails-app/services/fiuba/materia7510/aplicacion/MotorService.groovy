36ee004c30284bf5590b8cad77109fc03298226d
EstebanRodriguezUBA <rodriguezsigloxxi@gmail.com>
2018-05-10 14:45:59 -0300

    _Se agrega encapsulacion de query-counter, consulta de contador funcion.

--- MonitorEventos/multiproyecto/monitorApi/grails-app/services/fiuba/materia7510/aplicacion/MotorService.groovy
+++ MonitorEventos/multiproyecto/monitorApi/grails-app/services/fiuba/materia7510/aplicacion/MotorService.groovy
@@ -14,7 +14,7 @@ class MotorService {
 	static final def procesar_datos_fn 	= "procesar_datos"
 	static final def process_data_dropping_signals_fn = "process_data_dropping_signals"
 	static final def inicializarProcesarSennalizar_fn = "agregar_reglas_procesar_datos_emitir_sennales"
-	
+	static final def consultar_contador_fn				= "consultar_contador"
 	
 	
 	static def cargar_Recursos_para_ejecucion (String ns,String funcion_invocada){
@@ -38,6 +38,11 @@ class MotorService {
 
 	static def process_data_dropping_signals(def state, def dato){
 		ClojureProcesador.invocar (cargar_Recursos_para_ejecucion(ns,process_data_dropping_signals_fn), state, dato)
+
+	}
+	
+	static def consultar_contador(def state, def nombre_Contador, String[] argumento){
+		ClojureProcesador.invocar (cargar_Recursos_para_ejecucion(ns,consultar_contador_fn), state, nombre_Contador, argumento)
 
 	}
 }	
